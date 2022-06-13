5f13b9309f60768f109752fdb26e9c16dda80714
Jean Fraga <jeandevjs@gmail.com>
2022-06-13 17:55:09 -0300

    feat: update  server

--- web/server.js
+++ web/server.js
@@ -5,7 +5,9 @@ const app = express()
 
 const port = process.env.PORT || 3000; 
 
-app.use('/*', express.static( path.resolve(__dirname, './app/dist')))
-
+app.use( express.static( path.resolve(__dirname, './app/dist')))
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname , "app/client/build", "index.html"));
+   });
 
 app.listen(port, (()=> console.log(`Front run on ${port} 🚀`)))
