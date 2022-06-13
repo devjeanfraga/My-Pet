0ae7e391d2c89da5c87a4db5de7b21c1feeace23
Jean Fraga <jeandevjs@gmail.com>
2022-06-13 19:53:57 -0300

    update

--- web/server.js
+++ web/server.js
@@ -5,9 +5,9 @@ const app = express()
 
 const port = process.env.PORT || 3000; 
 
-app.use( express.static( path.resolve(__dirname, './build')))
-app.get("/", function (req, res) {
-  res.sendFile(path.resolve(__dirname , "build", "index.html"));
-   });
+app.use( '/', express.static( path.resolve(__dirname, './build')))
+// app.get("/", function (req, res) {
+//   res.sendFile(path.resolve(__dirname , "build", "index.html"));
+//    });
 //
 app.listen(port, (()=> console.log(`Front run on ${port} 👍 `)))
