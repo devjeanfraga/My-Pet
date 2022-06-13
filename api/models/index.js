b019a1e8fcc551027d3a68d8d32a6e7b7e714036
Jean Fraga <jeandevjs@gmail.com>
2022-06-13 15:13:05 -0300

    chore setup db

--- api/models/index.js
+++ api/models/index.js
@@ -5,14 +5,24 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.JAWSDB_URL) {
+
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(  
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 }
 
 fs
